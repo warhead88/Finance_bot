50f49c714057c74eae878450f9c2b9d12ce36c94
w88 <krippach147@gmail.com>
2025-10-05 19:24:07 +0500

    Updated start command
    User will be added to "users" database when he enter start command

--- handlers/start.py
+++ handlers/start.py
@@ -1,8 +1,24 @@
 from aiogram import Router, types
 from aiogram.filters import Command
 
+from sqlalchemy import select
+
+from db.db import get_async_session
+from db.tables import User
+
 router = Router()
 
 @router.message(Command("start"))
 async def start(message: types.Message):
-    await message.answer("Привет! Это бот-финансовый менеджер.")
+    async with get_async_session() as session:
+        result = await session.execute(
+            select(User).filter_by(id = message.from_user.id)
+        )
+        user = result.scalar_one_or_none()
+
+        if not user:
+            user = User(id=message.from_user.id)
+            session.add(user)
+            await message.answer("Привет! Это бот финансовый менеджер. Он поможет тебе справиться с финансами.")
+        else:
+            await message.answer("Для получения инструкций введи команду /help.")
